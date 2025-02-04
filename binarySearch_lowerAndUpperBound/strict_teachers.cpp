0a62c1eeba45dff18c3741590c900ea1289598a0
Mazen Azhary <mazen.mazen9850@gmail.com>
2025-02-05 01:33:23 +0200

    solved strict teachers problem

--- binarySearch_lowerAndUpperBound/strict_teachers.cpp
+++ binarySearch_lowerAndUpperBound/strict_teachers.cpp
@@ -11,49 +11,70 @@ void solve()
 {
     int n, m, q;
     cin >> n >> m >> q;
-    vector<int> b(n + 1, 0);
-    vector<int> a(q + 1, 0);
-    for (int i = 1; i < m + 1; i++)
+    vector<int> b(m, 0);
+    vector<int> a(q, 0);
+    for (int i = 0; i < m; i++)
     {
         cin >> b[i];
+        b[i]--;
     }
-    for (int i = 1; i < q + 1; i++)
+    for (int i = 0; i < q; i++)
     {
         cin >> a[i];
+        a[i]--;
     }
-    vector<int> distances(m + 1, 0);
 
-    for (int i = 1; i < q + 1; i++)
+    sort(b.begin(), b.end());
+    for (int i = 0; i < q; i++)
     {
         int initialPosition = a[i];
         // I will try to get 2 closest teachers by creating an array of distances between all teachers and david , then I will sort it and get the 2 closest teachers
-        for (int j = 1; j < m + 1; i++)
-        {
-            distances[j] = (b[j] - initialPosition);
-        }
 
         vector<int> closestTeachers(2, 0);
-        if (m > 2)
+        if (m > 1)
         {
-            sort(distances.begin(), distances.end());
-            closestTeachers[0] = lower_bound(distances.begin(), distances.end(), 0) - distances.begin() - 1;//highest negative (on david's left)
-            closestTeachers[1] = lower_bound(distances.begin(), distances.end(), 0) - distances.begin();//highest positive (on david's right)
+            closestTeachers[0] = lower_bound(b.begin(), b.end(), initialPosition) - b.begin() - 1; // highest negative (on david's left)
+            closestTeachers[1] = closestTeachers[0] + 1;                                           // highest positive (on david's right)
             // if two teachers are right or left of david we get closer one
 
-        // I have 3 cases , if david is between two teachers , or if we have one teacher and david is either closer to the left or the right
-            //closest teacher[1] == 0 means we care about closest teacher[0] only
-            
-                
+            if (closestTeachers[0] < 0)
+            { // no teacher on david's left
+                closestTeachers[0] = closestTeachers[1];
+                closestTeachers[1] = 0;
+            }
+            else if (closestTeachers[1] == m)
+            { // no teacher on david's right
+                closestTeachers[1] = 0;
+            }
 
+            // I have 3 cases , if david is between two teachers , or if we have one teacher and david is either closer to the left or the right
+            // closest teacher[1] == 0 means we care about closest teacher[0] only
+        }
+        // | | | | | |T1| | | | | || |D| | | | | | \ |  | | |T2
+
+        else
+        {
+            closestTeachers[0] = 0;
+        }
+        if (closestTeachers[1] == 0)
+        { // only one teacher and david is to the sides
+            int teacherPos = b[closestTeachers[0]];
+            if (initialPosition - teacherPos > 0)
+            {//david on right
+            cout << n - teacherPos -1<< '\n';
+            }
+            else
+            {//david on left
+                cout << teacherPos  << '\n';
+            }
         }
         else
         {
-            closestTeachers[0] = b[1];
+            cout << abs(b[closestTeachers[0]]  -b[closestTeachers[1]])/2 << '\n';
         }
-
     }
 }
-
+ 
 int main()
 {
     Mazen int t;
